cd473c4de60d8fe9823126ca33e925b7469f6edd
purebaba <huangdekai@gmail.com>
2021-01-11 16:24:16 +0800

    overwrite url

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var plist = require('simple-plist');
 
 module.exports = function(file, callback){
-  var certDl = new (require('cert-downloader'));
+  var certDl = new (require('cert-downloader'))({url:'https://www.apple.com/appleca/AppleIncRootCertificate.cer'});
   certDl.verify(file, function(error, output) {
     if(error){
       return callback(error);
